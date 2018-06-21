98c12adb12f161b07e37661d3fec559b2c02be2a
datvithanh98 <datvithanh98@gmail.com>
2018-06-21 16:36:22 +0700

    create queue, user_queue, customer_queue table

---
+++ database/migrations/2018_06_21_090923_create_queues_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQueuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('queues', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('service_name');
+            $table->string('status');
+            $table->string('max_people_count');
+            $table->timestamps();
+        });
+        DB::statement('ALTER TABLE queues ALTER COLUMN id SET DEFAULT uuid_generate_v4();');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('queues');
+    }
+}
---
+++ database/migrations/2018_06_21_093359_create_customer_queue_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerQueueTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_queue', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('customer_id');
+            $table->uuid('queue_id');
+            $table->timestamps();
+        });
+        DB::statement('ALTER TABLE customer_queue ALTER COLUMN id SET DEFAULT uuid_generate_v4();');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_queue');
+    }
+}
